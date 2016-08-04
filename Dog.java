36aed26f29212b7d76e7fb08bbd5aced87f5ff68
samli6479 <samli6479@gmail.com>
2016-08-04 14:32:19 -0400

    Updated Dog file and the main file Also include documentation

--- Dog.java
+++ Dog.java
@@ -2,7 +2,22 @@
  * Created by edward on 8/4/16.
  */
 public class Dog {
-    public static void makeNoise(){
-        System.out.println("Bark!");
+    public int size;  /* Instance variable*/
+
+    public Dog(int startSize){    /*Same as def __init__(self, StartSize) how to create a 
+                                  initialize the the new object of the class*/
+          this.size = startSize;
+    }
+
+    public void makeNoise(){   /*If the object is going to be invoked by given instance
+                                 then choose Non-static*/
+        if (size<10){
+           System.out.println("Horrific yapping");
+        
+        } else if (size < 30){
+           System.out.println("barl");
+        } else {
+          System.out.println("Bark!");
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        Dog.makeNoise();
+           Dog smallDog; /*Declaration of a dog variable*/
+           new Dog(20);  /*Instantiation of the Dog class as a Dog Object*/
+           smallDog = new Dog(5); /*Instantiation and Assignment*/
+           Dog hugeDog = new Dog(50); /*Declaration,Instantiation and Assignment*/
+           smallDog.makeNoise(); /*Invokation of the small dog's makeNoise method*/
+           hugeDog.makeNoise();  /*The dot here means we want to use a method of member of 
+                                 hugeDog*/
     }
 }
