827ac85c2feff15c132b62cd4f6377b5183728a7
samli6479 <samli6479@gmail.com>
2016-08-04 22:00:50 -0400

    Creates new folders and have a new StringDemo Class

---
+++ Lecture 1/StringDemo.java
@@ -0,0 +1,13 @@
+public class StringDemo{
+       
+       public static void main(String[] args){
+              String s = new String();
+
+              s = "Edward is learning java";
+              System.out.println(s);
+
+              s = s.toUpperCase(); /*Convert all to upper case*/
+              System.out.println(s);
+       }       
+
+}
